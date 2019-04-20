7152f558ff0cdf03fbadf3feb24e748187a10769
Peter Kotowski <bighoopee@gmail.com>
2019-04-20 16:54:56 -0700

    salut

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const http = require('http');
 
-function onRequest(request, response) {
+http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('wasssup?');
+    response.write('Salut, ca va?');
     response.end();
+}).listen(8888);
 
+
+
+function parlez(mot) {
+    console.log(mot);
 }
-http.createServer(onRequest).listen(8888);
+
+function execute(someFunction, value) {
+    someFunction(value);
+}
+
+execute(parlez, "salut");
