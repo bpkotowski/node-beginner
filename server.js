a16e28a769b81be9287375a1b125a2887c0852c1
Peter Kotowski <bighoopee@gmail.com>
2019-04-21 13:55:32 -0700

    received image message upload

--- server.js
+++ server.js
@@ -4,37 +4,12 @@ const url = require('url');
 
 function start(route, handle) {
     function onRequest(request, response) {
-        var postData = '';
         var pathname = url.parse(request.url).pathname;
-        console.log('request' +  pathname + 'received');
-        
-        request.setEncoding('utf8');
-
-        request.addListener('data', function(postDataChunk) {
-            postData = postDataChunk;
-            console.log('recieved POST data chunk' + postDataChunk + '');
-        });
-
-        request.addListener('end', function() {
-            route(handle, pathname, response, postData);
-
-        });
-        
-        
-       
+        console.log('request' + pathname + 'received');
+        route(handle, pathname, response, request);
     }
-    
     http.createServer(onRequest).listen(8888);
-     console.log('server is running')
-    
+    console.log('server is running')
+
 }
 exports.start = start;
-// function parlez(mot) {
-//     console.log(mot);
-// }
-
-// function execute(someFunction, value) {
-//     someFunction(value);
-// }
-
-// execute(parlez, "salut");
